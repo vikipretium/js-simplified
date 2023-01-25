b8c6be95fc503744d1bb00a0dc99f9f3c9077ce6
vikipretium <vighneshpasupathy@gmail.com>
2023-01-26 05:11:49 +0530

    add new lessons

--- script.js
+++ script.js
@@ -1,1 +1,86 @@
+// l6
 console.log("Hello World!")
+
+// l8 - variables
+let favNum = 1
+console.log(favNum)
+favNum = 4
+console.log(favNum)
+
+// l9 - numtypes
+let a = 1
+console.log(typeof a)
+
+let int = 2
+let float = 3
+
+console.log(typeof int)
+console.log(typeof float)
+
+let num1 = 1.2
+let num2 = 2.2
+
+console.log(num1 + num2) //check output
+
+// l10 - strings
+let string1 = "some words for string"
+console.log(string1)
+
+let word1 = "Hello"
+let word2 = "World!"
+
+console.log(word1 + " " + word2)
+
+// l11 - booleans
+let bvalue = true
+let bvalue1 = false
+
+console.log(typeof bvalue)
+console.log(typeof bvalue1)
+
+console.log(bvalue && bvalue1)
+console.log(bvalue || bvalue1)
+console.log(bvalue && !bvalue1)
+
+let happy = true
+console.log(happy)
+console.log(!happy)
+console.log(!!happy)
+
+// l12 - null and undefined
+let undefinedValue = undefined
+console.log(undefinedValue)
+
+let unassingnedValue
+console.log(unassingnedValue)
+
+unassingnedValue = null
+console.log(unassingnedValue)
+
+// l13 - comparison operator
+
+let value1 = 1
+let value2 = 2
+
+console.log(value1 == value2)
+console.log(value1 != value2)
+console.log(value1 < value2)
+console.log(value1 > value2)
+console.log(value1 <= value2)
+console.log(value1 >= value2)
+
+value1 = null
+value2 = undefined
+
+console.log("null + undefined: " + (value1 == value2))
+
+// l14 - garbage collector
+// unused variables will be deleted, if its not used in any line of the program anymore.
+
+// l15 - comments
+/*
+ for multi line comment use 
+ use these in line 81 and 83
+*/
+
+// l16 - 
