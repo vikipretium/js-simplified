0ae1ce759192604c6d391fbd233057d0f89656b4
vikipretium <vighneshpasupathy@gmail.com>
2023-01-26 06:08:55 +0530

    functions, scoping, passing

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
 // l6
-console.log("Hello World!")
+// console.log("Hello World!")
 
 // l8 - variables
-let favNum = 1
-console.log(favNum)
-favNum = 4
-console.log(favNum)
+// let favNum = 1
+// console.log(favNum)
+// favNum = 4
+// console.log(favNum)
 
 // l9 - numtypes
-let a = 1
-console.log(typeof a)
+// let a = 1
+// console.log(typeof a)
 
-let int = 2
-let float = 3
+// let int = 2
+// let float = 3
 
-console.log(typeof int)
-console.log(typeof float)
+// console.log(typeof int)
+// console.log(typeof float)
 
-let num1 = 1.2
-let num2 = 2.2
+// let num1 = 1.2
+// let num2 = 2.2
 
-console.log(num1 + num2) //check output
+// console.log(num1 + num2) //check output
 
 // l10 - strings
-let string1 = "some words for string"
-console.log(string1)
+// let string1 = "some words for string"
+// console.log(string1)
 
-let word1 = "Hello"
-let word2 = "World!"
+// let word1 = "Hello"
+// let word2 = "World!"
 
-console.log(word1 + " " + word2)
+// console.log(word1 + " " + word2)
 
 // l11 - booleans
-let bvalue = true
-let bvalue1 = false
+// let bvalue = true
+// let bvalue1 = false
 
-console.log(typeof bvalue)
-console.log(typeof bvalue1)
+// console.log(typeof bvalue)
+// console.log(typeof bvalue1)
 
-console.log(bvalue && bvalue1)
-console.log(bvalue || bvalue1)
-console.log(bvalue && !bvalue1)
+// console.log(bvalue && bvalue1)
+// console.log(bvalue || bvalue1)
+// console.log(bvalue && !bvalue1)
 
-let happy = true
-console.log(happy)
-console.log(!happy)
-console.log(!!happy)
+// let happy = true
+// console.log(happy)
+// console.log(!happy)
+// console.log(!!happy)
 
 // l12 - null and undefined
-let undefinedValue = undefined
-console.log(undefinedValue)
+// let undefinedValue = undefined
+// console.log(undefinedValue)
 
-let unassingnedValue
-console.log(unassingnedValue)
+// let unassingnedValue
+// console.log(unassingnedValue)
 
-unassingnedValue = null
-console.log(unassingnedValue)
+// unassingnedValue = null
+// console.log(unassingnedValue)
 
 // l13 - comparison operator
 
-let value1 = 1
-let value2 = 2
+// let value1 = 1
+// let value2 = 2
 
-console.log(value1 == value2)
-console.log(value1 != value2)
-console.log(value1 < value2)
-console.log(value1 > value2)
-console.log(value1 <= value2)
-console.log(value1 >= value2)
+// console.log(value1 == value2)
+// console.log(value1 != value2)
+// console.log(value1 < value2)
+// console.log(value1 > value2)
+// console.log(value1 <= value2)
+// console.log(value1 >= value2)
 
-value1 = null
-value2 = undefined
+// value1 = null
+// value2 = undefined
 
-console.log("null + undefined: " + (value1 == value2))
+// console.log("null + undefined: " + (value1 == value2))
 
 // l14 - garbage collector
 // unused variables will be deleted, if its not used in any line of the program anymore.
@@ -83,4 +83,33 @@ console.log("null + undefined: " + (value1 == value2))
  use these in line 81 and 83
 */
 
-// l16 - 
+// l16 - functions and scoping
+function sayHello() {
+  console.log("Hello")
+}
+sayHello()
+
+function sum(a, b) {
+  return a + b
+}
+
+let number1 = 1
+let number2 = 2
+let sum1 = sum(number1, number2)
+console.log(sum1)
+
+function helloName(name) {
+  return "Hello " + name
+}
+console.log(helloName("v"))
+
+// l17 - pass function to another function
+function func1(variable1) {
+  console.log(variable1)
+}
+
+function func2(funcpass) {
+  funcpass("hello")
+}
+
+func2(func1)
